c0769cfa91e9aa73d1016b3b050a18ff64ad1c89
Ricky <rikkii.takamoto7589@gmail.com>
2026-05-13 11:41:10 +0900

    pin配置を定数定義するconfigファイルとピン配置を定義
     On branch dev
     Changes to be committed:
    	new file:   include/pins_config.hpp

---
+++ EDL/include/pins_config.hpp
@@ -0,0 +1,36 @@
+#ifndef PINS_CONFIG_HPP
+    #define PINS_CONFIG_HPP
+    
+    #include "pins_arduino.h"
+    //Arduinoのピン位置を定義しているファイル。
+
+    //各指標表示時の信号先セレクタピンの定義
+    #define TEMPERATURE_INDEX 0
+    #define HUMIDITY_INDEX 1
+    #define NOISE_INDEX 2
+    #define CO2_INDEX 3
+    #define OVERALL_INDEX 4
+
+    //Output bus に使うvalueピンの定義
+    #define OUTPUT_BIT0 5
+    #define OUTPUT_BIT1 6
+    #define OUTPUT_BIT2 7
+
+    
+    class IndexSelector{
+        public:
+            static constexpr int TEMPERATURE =TEMPERATURE_INDEX;
+            static constexpr int HUMIDITY = HUMIDITY_INDEX;
+            static constexpr int NOISE = NOISE_INDEX;
+            static constexpr int CO2 = CO2_INDEX;
+            static constexpr int OVERALL = OVERALL_INDEX;
+    };
+
+    class OutputBus{
+        public:
+            static constexpr int BIT0 = OUTPUT_BIT0;
+            static constexpr int BIT1 = OUTPUT_BIT1;
+            static constexpr int BIT2 = OUTPUT_BIT2;
+    };
+
+#endif //PINS_CONFIG_HPP
