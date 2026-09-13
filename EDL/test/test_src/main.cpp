1d7e3cbd79c412812b67f6b1679d9b6ddaa3f6de
Ricky_win <rikkii.takamoto7589@gmail.com>
2026-09-13 23:27:38 +0900

    TestコードをGoogleTest実装しようとしたが、設計段階であまりそれを考えていなかったため難しくなった。これは其れの残骸である為restore時などは特に気にしなくてよい。（削除するとログとして残らなくなるため必要なくなったと確定するまでは一度残しておく）

---
+++ EDL/test/test_src/main.cpp
@@ -0,0 +1,75 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include <DFRobot_ENS160.h>
+#include<cpp_helper.hpp>
+
+#include"interface_main.hpp"
+#include"pins_config.hpp"
+#include"err.hpp"
+
+InterFace outputInterFace;
+
+
+void setup(){
+    Serial.begin(9600);
+    delay(5000);
+    Serial.println("Test");
+    Serial.print("\ttest Interface:"); 
+    outputInterFace.begin();
+    Serial.println("\tOK");
+
+}
+
+void loop(){
+    InterFace outputInterFace;
+    String inputIndex;
+    int indexBus;
+    String inputValue;
+    int valueBus;
+    while(true){
+    Serial.println("Prease Write the Index:");
+    while(Serial.available() == 0)  delay(100);
+    if(Serial.available()>0)    inputIndex = Serial.readStringUntil('\n');
+
+    if(inputIndex == "温度")        indexBus = IndexSelector::TEMPERATURE;
+    else if(inputIndex == "湿度")   indexBus = IndexSelector::HUMIDITY;
+    else if(inputIndex == "CO2")    indexBus = IndexSelector::CO2;
+    else if(inputIndex == "騒音")   indexBus = IndexSelector::NOISE;
+    else if(inputIndex == "総合")   indexBus = IndexSelector::OVERALL;
+    else{
+        Serial.println("Input err");
+        continue;
+    }
+    break;
+    }
+    while (true){
+    Serial.println("Prease Write the Value:");
+    while(Serial.available() == 0)  
+    if(Serial.available()>0)    inputValue = Serial.readStringUntil('\n');
+
+    if(!TypeConverter::checkTypeToInt(inputValue.c_str())){
+        Serial.println("Input err");
+        continue;
+    }
+    valueBus = inputValue.toInt();
+    Serial.println(valueBus);
+    if(valueBus >= 8 || valueBus <= 0){
+        Serial.println("Input err");
+        continue;
+    }
+
+    break;
+    }
+
+    Serial.println("test output");
+
+    if((valueBus >> 0) & 1)    Serial.println("BIT0");
+    if((valueBus >> 1) & 1)    Serial.println("BIT1");
+    if((valueBus >> 2) & 1)    Serial.println("BIT2");
+
+    ErrCode::checkErr(outputInterFace.outputNumberOfBus(indexBus,valueBus),&Serial);
+    delay(100000);
+   delay(100000);
+    
+
+}
