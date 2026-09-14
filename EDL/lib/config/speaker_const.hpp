4f76edc462449a10219919924ef69d0518f81b74
Ricky_win <rikkii.takamoto7589@gmail.com>
2026-09-14 11:37:44 +0900

    アナログの電圧値をスピーカーのdB表記に直す関数の追加 <info> 	今回はデータシートを用いた近似でしか構築していないので正確な値はboardのゲイン等を元に調節しなおす必要がある。また式についても本当にそうなのか疑念が残る為調整したい。  On branch dev  Changes to be committed: 	new file:   EDL/lib/config/speaker_const.hpp 		boardのゲインを定数宣言する為のfileになってしまっているがスピーカー全体の定数を本当はここに統一すべきかも。 	modified:   EDL/lib/hardware/include/analog_device.hpp 		アナログ電圧→スピーカーdBの変換関数vltageToVolume関数の宣言 	modified:   EDL/lib/hardware/src/analog_device.cpp 		voltageToVolume関数の実態を構築

---
+++ EDL/lib/config/speaker_const.hpp
@@ -0,0 +1,9 @@
+#ifndef SPEAKER_CONST_HPP
+#define SPEAKER_CONST_HPP
+
+    class speakerConst{
+    public:
+        static const float boardGain = 1;
+    };
+
+#endif //SPEAKER_CONST_HPP
--- EDL/lib/hardware/src/analog_device.cpp
+++ EDL/lib/hardware/src/analog_device.cpp
@@ -1,6 +1,11 @@
 #include"analog_device.hpp"
+#include"speaker_const.hpp"
 
 float AnalogReader::convertToVoltage(int rawVoltage){
     return ((5.0/1023.0)*rawVoltage);
     //分解能をもとにしきい値を電圧に変換する
+}
+
+static float voltageToVolume(float voltage){
+    return(94 + 20*log10(voltage/(speakerConst::boardGain*6.31e-3)));
 }
