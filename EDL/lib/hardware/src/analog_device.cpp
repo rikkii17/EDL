d1f67c748c9abf3978d83c04dffb005a765333d0
Ricky_win <rikkii.takamoto7589@gmail.com>
2026-09-21 18:57:53 +0900

    ミスったところ(cppfile）にstatic の前置詞を入れていたのでその修正  On branch dev  Changes to be committed: 	modified:   EDL/lib/hardware/src/analog_device.cpp

--- EDL/lib/hardware/src/analog_device.cpp
+++ EDL/lib/hardware/src/analog_device.cpp
@@ -5,7 +5,6 @@ float AnalogReader::convertToVoltage(int rawVoltage){
     return ((5.0/1023.0)*rawVoltage);
     //分解能をもとにしきい値を電圧に変換する
 }
-
-static float AnalogReader::voltageToVolume(float voltage){
+float AnalogReader::voltageToVolume(float voltage){
     return(94 + 20*log10(voltage/(speakerConst::boardGain*6.31e-3)));
 }
